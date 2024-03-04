0e3e45a65db5f486084a7f56cb6752a1ab54a6e7
PARTENORGROUP\jdidier <jdidier@partenorgroup.net>
2024-03-04 17:16:01 +0100

    feat (Link): master - [A] Link registery

--- ParserModule/Parser/PHP/StructureParser.py
+++ ParserModule/Parser/PHP/StructureParser.py
@@ -1,6 +1,6 @@
 from ParserModule.Parser.PHP import Parser
 from Registry.Registry import Registry
-from Registry.RegistryModule.StructuralRegistry.Structure import (RegistryClass, RegistryInterface, RegistryEnum)
+from Registry.RegistryModule.RelationalRegistry.Link import (Heritage, Implementation)
 import re
 
 class StructureParser( Parser ):
@@ -8,28 +8,31 @@ class StructureParser( Parser ):
         print('Initialisation StructureParser')
         print('└────────────────────────────│')
 
-         
+
     def parse( self, line: str, registry: Registry ):
         print('StructureParser -----> [START]')
-        
+
         structure_pattern = re.compile(r"""(?P<type>class|interface|enum)\s+(?P<name>\w+)\s*(?:extends\s+(?P<extends>\w+)\s*)?(?:implements\s+(?P<implements>[\w\s,]+))?""", re.MULTILINE | re.DOTALL)
 
         for match in re.finditer(structure_pattern, line):
-            
+
             # Configuration du type de structure en fonction de l'entité déterminée
             structure_element = "Registry" + match.group('type').capitalize() # Transforme 'class' en 'RegistryClass', etc.
             structure_element.set_name(match.group('name'))
-            
-            
+
+
             # TODO : Configuration de l'héritage et des implémentations
             if match.group('extends') or match.group('implements'):
-                heritage_element = HeritageRegistry()
                 if match.group('extends'):
-                    heritage_element.set_extends(match.group('extends'))
+                    heritage_element = Heritage()
+                    heritage_element.set_source(match.group('name'))
+                    heritage_element.set_destination(match.group('extends'))
                 if match.group('implements'):
-                    heritage_element.set_implements(match.group('implements'))
-            
+                    Implementation_element = Implementation()
+                    Implementation_element.set_source(match.group('name'))
+                    Implementation_element.set_destination(match.group('implements'))
+
             registry.get_active_element().add_child(structure_element)
             registry.set_active_element()
-            
+
         print('StructureParser -----> [DONE]')
--- Registry/RegistryModule/RelationalRegistry/Link.py
+++ Registry/RegistryModule/RelationalRegistry/Link.py
@@ -5,18 +5,54 @@ class Link( ABC, RelationalElement ):
 
     def __init__( self ):
        pass
-    
+
+    @abstractmethod
     def get_source( self ):
         pass
-    
+
+    @abstractmethod
     def get_Destination( self ):
         pass
 
-class Association(Link):
+class Composition( Link ):
     def __init__( self ):
        pass
-    
+
     def buildUml( self ):
+        return self.get_source() + ' *-- ' + self.get_Destination()
+
+class Heritage( Link ):
+    def __init__( self ):
         pass
-    
-   
+
+    def buildUml( self ):
+        return self.get_source() + ' --|> ' + self.get_Destination()
+
+
+class Association( Link ):
+    def __init__( self ):
+        pass
+
+    def buildUml( self ):
+        return self.get_source() + ' --> ' + self.get_Destination()
+
+class Aggregation( Link ):
+    def __init__( self ):
+        pass
+
+    def buildUml( self ):
+        return self.get_source() + ' o--> ' + self.get_Destination()
+
+class Implementation( Link ):
+    def __init__( self ):
+        pass
+
+    def buildUml( self ):
+        return self.get_source() + ' ..|> ' + self.get_Destination()
+
+class Dependance( Link ):
+    def __init__( self ):
+        pass
+
+    def buildUml( self ):
+        return self.get_source() + ' ..> ' + self.get_Destination()
